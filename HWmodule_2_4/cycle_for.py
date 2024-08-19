961284cf40a3a9a024357e3987dfd395d5d779e7
NikitaBelikov <belsta.nik@gmail.com>
2024-08-19 17:18:32 +0300

    imported the math module, changed the loop

--- HWmodule_2_4/cycle_for.py
+++ HWmodule_2_4/cycle_for.py
@@ -1,12 +1,14 @@
-numbers = [-11, 2, -3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+import math
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
 for i in numbers:
-    if -10 < i < 10:
+    if math.sqrt(i).is_integer() == False:
         primes.append(i)
     else:
         not_primes.append(i)
 
-print(primes)
-print(not_primes)
+print("Prime numbers:", primes)
+print("Not prime numbers:", not_primes)
