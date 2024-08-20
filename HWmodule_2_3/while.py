6962210feb3c843008c84d6e56ec7c0a5ff52f06
NikitaBelikov <belsta.nik@gmail.com>
2024-08-20 16:33:34 +0300

    include condition with 0

--- HWmodule_2_3/while.py
+++ HWmodule_2_3/while.py
@@ -1,8 +1,13 @@
+from importlib.metadata import pass_none
+
 numbers = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while i < len(numbers) and numbers[i] >= 0:
+    if 0 in numbers:
+        numbers.remove(0)
     print(numbers[i])
     i += 1
+
 
 
 
